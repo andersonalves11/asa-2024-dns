300930286ddc6c5d2b17c8bafe50dd177abb6aac
andersonalves11 <160679362+andersonalves11@users.noreply.github.com>
2024-07-23 11:37:17 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# asa-2024-dns
+# Disciplina dev ASA 2024.1
+Repositorio para a disciplina de ASA 2024.1
+
+### Aula de exmplo de como versionar códicos
+
+### Como baixar o repositório
+1. git clone <REPO>
+
+### Como enviar para o repo central
+
+git add .
+git commit -m "msg"
+git push
---
+++ proxy-nginx/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## Build
+docker build -t web01 ./web01
+docker build -t web02 ./web02
+docker build -t web03 ./web03
+docker build -t proxy ./proxy
+
+## Run
+docker network create -d bridge asa-net
+docker run -d --net=asa-net --name web01 web01
+docker run -d --net=asa-net --name web02 web02
+docker run -d --net=asa-net --name web03 web03
+docker run -dp 8081:80 --net=asa-net --name web01 web01
+docker run -dp 8082:80 --net=asa-net --name web02 web02
+docker run -dp 8083:80 --net=asa-net --name web03 web03
+docker run -dp 8090:80 --net=asa-net --name proxy proxy
+
+
